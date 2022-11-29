8ad961345d4395f313cd48e4ae6ac0a1c48c3513
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-29 16:28:22 +0530

    [Siddharth]Add:Create regex pattern to validate pincode

---
+++ ProgrammingWithRegex/PostalCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProgrammingWithRegex
+{
+    public class PostalCode
+    {
+        const string PIN_CODE_PATTERN = "^[0-9]{6}$";
+        public void TestPinCode(string input)
+        {
+            if (Regex.IsMatch(input, PIN_CODE_PATTERN))
+                Console.WriteLine("Pin Code Matches");
+            else
+                Console.WriteLine("Pin Code Regex Failed");
+        }
+    }
+}
--- ProgrammingWithRegex/Program.cs
+++ ProgrammingWithRegex/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ProgrammingWithRegex
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Programming With Regex Problem Statements");
+            Console.WriteLine("Select Below Mention Options");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(" 1. ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:    
+                        PostalCode code = new PostalCode();
+                        string opt = Convert.ToString(Console.ReadLine());
+                        code.TestPinCode(opt);
+                        break;
+                    default:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
