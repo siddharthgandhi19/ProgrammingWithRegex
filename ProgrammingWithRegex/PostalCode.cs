85f7725f6795b5403bf96a2de72485d5b2ad1308
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-29 16:47:41 +0530

    [Siddharth]Add: Make sure 400 088 is also valid along with 400088

--- ProgrammingWithRegex/PostalCode.cs
+++ ProgrammingWithRegex/PostalCode.cs
@@ -10,7 +10,7 @@ namespace ProgrammingWithRegex
     public class PostalCode
     {
         const string PIN_CODE_PATTERN = "^[0-9]{6}$";
-        const string PIN_CODE_PATTERN2 = "^[0-9]{6}$";
+        string PIN_CODE_PATTERN1 = "^[0-9]{3}[ ]{0,1}[0-9]{3}$";
         public void TestPinCode(string input)
         {
             if (Regex.IsMatch(input, PIN_CODE_PATTERN))
@@ -20,7 +20,21 @@ namespace ProgrammingWithRegex
         }
         public void TestPinCode2(string input)
         {
-            if (Regex.IsMatch(input, PIN_CODE_PATTERN2))
+            if (Regex.IsMatch(input, PIN_CODE_PATTERN))
+                Console.WriteLine("Pin Code Matches");
+            else
+                Console.WriteLine("Pin Code Regex Failed");
+        }
+        public void TestPinCode3(string input)
+        {
+            if (Regex.IsMatch(input, PIN_CODE_PATTERN))
+                Console.WriteLine("Pin Code Matches");
+            else
+                Console.WriteLine("Pin Code Regex Failed");
+        }
+        public void TestPinCode4(string input)
+        {
+            if (Regex.IsMatch(input, PIN_CODE_PATTERN1))
                 Console.WriteLine("Pin Code Matches");
             else
                 Console.WriteLine("Pin Code Regex Failed");
--- ProgrammingWithRegex/Program.cs
+++ ProgrammingWithRegex/Program.cs
@@ -10,24 +10,33 @@ namespace ProgrammingWithRegex
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine(" 1. Check pattern for pincode\n 2. Restrict pincode pattern at begining ");
+                Console.WriteLine(" 1. Check pattern for pincode\n 2. Restrict pincode pattern at begining\n 3. Restrict pincode pattern at End\n 4. Pincode With Space Between  ");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:    
                         PostalCode code = new PostalCode();
+                        Console.WriteLine("Enter Pincode");
                         string opt = Convert.ToString(Console.ReadLine());
                         code.TestPinCode(opt);
                         break;
                     case 2:
                         PostalCode code1 = new PostalCode();
+                        Console.WriteLine("Enter Pincode");
                         string opt1 = Convert.ToString(Console.ReadLine());
                         code1.TestPinCode2(opt1);
                         break;
-                        case 3:
+                   case 3:
                         PostalCode code2 = new PostalCode();
+                        Console.WriteLine("Enter Pincode");
                         string opt2 = Convert.ToString(Console.ReadLine());
                         code2.TestPinCode3(opt2);
+                        break;
+                    case 4:
+                        PostalCode code3 = new PostalCode();
+                        Console.WriteLine("Enter Pincode");
+                        string opt3 = Convert.ToString(Console.ReadLine());
+                        code3.TestPinCode4(opt3);
                         break;
                     default:
                         flag = false;
